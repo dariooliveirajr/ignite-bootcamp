b554ebccefe76f67de6c2ecbdcdf0542aad38ee8
Dario de Oliveira Junior <dariooljr@gmail.com>
2021-03-31 20:45:16 -0300

    Refactor Dir

--- rentx/src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ rentx/src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,13 +1,14 @@
 import { getRepository, Repository } from "typeorm";
 
-import { Specification } from "../../entities/Specification";
 import {
-  ISpecificationsRepository,
   ICreateSpecificationDTO,
-} from "../ISpecificationsRepository";
+  ISpecificationsRepository,
+} from "@modules/cars/repositories/ISpecificationsRepository";
+
+import { Specification } from "../entities/Specification";
 
 class SpecificationsRepository implements ISpecificationsRepository {
-  private repository: Repository<Specification>;
+  private repository: Repository<Specificationtion>;
 
   constructor() {
     this.repository = getRepository(Specification);
